47ce08195c552c597ac4a686968f440a64d0f70d
ChernyhAnastacia <chernyh.anastacia@gmail.com>
2022-02-17 15:57:16 +0300

    add:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =function check(str, bracketsConfig) {
+    str = str.split('');
+    for (let i = 1; i < str.length; i++){
+        let brackets = [str[i-1], str[i]];
+        for (let j = 0; j < bracketsConfig.length; j++ ) {
+            let array = bracketsConfig[j];
+            if ((brackets[0] === array[0]) && (brackets[1] === array[1])) {
+                str.splice(i-1 , 2);
+                i = i - 2;
+            }
+        }
+    }
+    return (!(str && str.length !== 0));
 }
